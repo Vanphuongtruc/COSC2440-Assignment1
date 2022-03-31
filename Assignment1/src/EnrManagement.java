f0a3a05a446e9bf2a1e7818de7660a4164b45d89
Vanphuongtruc <57283544+Vanphuongtruc@users.noreply.github.com>
2022-03-31 16:00:51 +0700

    enrolment management

---
+++ Assignment1/src/EnrManagement.java
@@ -0,0 +1,19 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class EnrManagement {
+}
+interface ManagementSystem{
+    static void addEnrolment(ArrayList<Enrollment>enrList,
+                             ArrayList<Student> students,
+                             ArrayList<Course> courses,
+                             String[] semesters){
+        boolean checker = true;
+        Student addStudent = null;
+        Course addCourse = null;
+        //check if input exist
+        while (checker) {
+            
+
+    }
+}
